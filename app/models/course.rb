4bc561773ed42b37d0e734cdcd06b00a04fdef87
Kevin Liu <kliu33@buffalo.edu>
2022-12-08 17:02:30 -0500

    finish associations

--- app/models/course.rb
+++ app/models/course.rb
@@ -9,16 +9,30 @@
 #  created_at    :datetime         not null
 #  updated_at    :datetime         not null
 #
+#
+# For every foreign key, you have to make a belongs_to
 class Course < ApplicationRecord
 
     validates :name, presence: true
-    validates :prereq_id, presence: false
-    validates :instructor_id, presence: true
 
     has_many :enrollments,
         primary_key: :id,
         foreign_key: :course_id,
         class_name: :Enrollment
 
-    # has_many :  
+    has_many :enrolled_students,
+        through: :enrollments,
+        source: :student
+    
+    belongs_to :prerequisite,
+        primary_key: :id,
+        foreign_key: :prereq_id,
+        class_name: :Course,
+        optional: true
+
+    belongs_to :instructor,
+        primary_key: :id,
+        foreign_key: :instructor_id,
+        class_name: :User
+        
 end
